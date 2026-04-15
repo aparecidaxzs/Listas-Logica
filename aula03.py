b8e1cb7b1019bb3fd1d2a66d23bacef42799e4ca
Aparecida Oliveira <145566499+aparecidaxzs@users.noreply.github.com>
2026-04-15 11:35:55 -0300

    Add files via upload

---
+++ aula03.py
@@ -0,0 +1,17 @@
+value_a = int(input("Digite um numero inteiro: "))
+value_b = int(input("Digite outro numero inteiro: "))
+
+# comentario
+print(f"{value_a} + {value_b} → {value_a + value_b}")
+print(f"{value_a} * {value_b} → {value_a * value_b}")
+print(f"{value_a} - {value_b} → {value_a - value_b}")
+print(f"{value_a} / {value_b} → {value_a / value_b}")
+
+#oto comentario
+value_c = int(input("Digite um numero inteiro: "))
+value_d = int(input("Digite outro numero inteiro: "))
+
+print(f"{value_c} > {value_d} → {value_c > value_d}")
+print(f"{value_c} <= {value_d} → {value_c <= value_d}")
+print(f"{value_c} != {value_d} → {value_c != value_d}")
+print(f"{value_c} == {value_d} → {value_c == value_d}")
