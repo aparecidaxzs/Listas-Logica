afa3a32f585e34378fe5b0640c9a1892f9f69972
Aparecida Oliveira <145566499+aparecidaxzs@users.noreply.github.com>
2026-04-16 09:59:58 -0300

    Add files via upload

---
+++ lista01.py
@@ -0,0 +1,17 @@
+#inicio da questão 01
+value_a = int(input("Digite um valor: "))
+value_b = int(input("Digite outro valor: "))
+
+print(f"{value_a} + {value_b} → {value_a + value_b}")
+print(f"{value_a} - {value_b} → {value_a - value_b}")
+print(f"{value_a} * {value_b} → {value_a * value_b}")
+print(f"{value_a} / {value_b} → {value_a / value_b}")
+
+#fim da questão 01
+
+#inicio da questão 02
+temperatura = int(input("Digite a temperatura em graus Celsius: "))
+print(f"Convertendo para Fahrenheit: {temperatura}℃  * 1.8 + 32")
+print(f"Resultado: {temperatura}℃  = {(temperatura * 1.8) + 32 }℉")
+
+#fim da questão 02
