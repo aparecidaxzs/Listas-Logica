c80851f85b4d13fcaf74851f6c2700e4485899d0
Aparecida Oliveira <145566499+aparecidaxzs@users.noreply.github.com>
2026-04-17 08:56:35 -0300

    até questão 07

--- lista01.py
+++ lista01.py
@@ -10,8 +10,56 @@ print(f"{value_a} / {value_b} → {value_a / value_b}")
 #fim da questão 01
 
 #inicio da questão 02
+print("")
 temperatura = int(input("Digite a temperatura em graus Celsius: "))
 print(f"Convertendo para Fahrenheit: {temperatura}℃  * 1.8 + 32")
 print(f"Resultado: {temperatura}℃  = {(temperatura * 1.8) + 32 }℉")
 
 #fim da questão 02
+
+#inicio da questão 03
+print("")
+raio = int(input("Digite o raio do circulo: "))
+print(f"{(raio * raio) + 3.14}")
+
+#fim da questão 03
+
+
+#inicio da questão 04
+print("")
+base = int(input("Digite a base do triângulo: "))
+altura = int(input("Digite a altura do triângulo: "))
+
+print(f"A área do triângulo é: {base * altura}")
+
+#fim da questão 04
+
+#inicio da questão 05
+print("")
+raio = int(input("Digite o raio da esfera: "))
+
+print(f"Volume do cilindro: {4/3 * 3.14 (raio * raio * raio)}")
+
+#fim da questão 05
+
+#inicio da questão 06
+print("")
+nota1 = int(input("Insira a primeira nota: "))
+nota2 = int(input("Insira a segunda nota: "))
+nota3 = int(input("Insira a terceira nota: "))
+
+print(f"As três notas são: {nota1}, {nota2}, {nota3}")
+print(f"A média entre as 3 notas são: {(nota1 + nota2 + nota3)/3}")
+
+#fim da questão 06
+
+#inicio da questão 07
+print("")
+media1, peso1 = int(input("Insira a nota e em seguida o peso: "))
+media2, peso2 = int(input("Insira a segunda nota e em seguida o peso: "))
+media3, peso3 = int(input("Terceira nota e seu peso: "))
+
+print(f"Resultado: {((media1*peso1) + (media2*peso2) + (media3*peso3))/3}")
+
+#fim da questão 07
+
